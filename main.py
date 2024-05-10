4f5a6da6b8ba450795d47a2e7993fad81b0f12ca
navoni1024 <radec8402@gmail.com>
2024-05-10 19:32:25 +0800

    add embed help page

--- main.py
+++ main.py
@@ -5,6 +5,18 @@ import os
 import asyncio
 from discord.ext import commands
 
+class MyHelpCommand(commands.MinimalHelpCommand):
+	async def send_pages(self):
+		destination = self.get_destination()
+		pic = discord.File("SOYO.jpg")
+		embed=discord.Embed(title="買夠的指令:", url="")
+		embed.add_field(name="@買夠", value="輸出一張圖", inline=False)
+		embed.add_field(name="@買夠 g [num]", value="產生num秒的gif圖片，預設5秒。最高十秒", inline=True)
+		embed.add_field(name="", value="[源碼](https://github.com/navoni1024/Mygo_random_frame_bot)", inline=False)
+		embed.set_footer(text="*GIF太大的話，在discord中有可能不會動。")
+		embed.set_image(url="attachment://SOYO.jpg")
+		await destination.send(file=pic, embed=embed)
+
 async def load_extensions():
 	for filename in os.listdir('./cogs'):
 		if filename.endswith('.py'):
@@ -16,6 +28,7 @@ with open('setting.json','r',encoding='utf8') as jfile:
 intents = discord.Intents.default()
 intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(jdata['prefix']), intents=intents)
+bot.help_command = MyHelpCommand()
 
 asyncio.run(load_extensions())
 
