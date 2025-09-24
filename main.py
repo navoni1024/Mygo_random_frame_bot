b19d464f137a9b1c13fbf770e8b1d55a20973315
navoni1024 <radec8402@gmail.com>
2025-09-25 04:40:22 +0800

    adjust intent

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class MyHelpCommand(commands.MinimalHelpCommand):
 		pic = discord.File("SOYO.jpg")
 		embed=discord.Embed(title="買夠的指令:", url="")
 		embed.add_field(name="@買夠", value="輸出一張圖", inline=False)
-		embed.add_field(name="@買夠 g [num]", value="產生num秒的gif圖片，預設5秒。最高十秒", inline=True)
+		embed.add_field(name="@買夠 g [num]", value="產生num秒的gif圖片，預設3秒。最高10秒", inline=True)
 		embed.add_field(name="", value="[源碼](https://github.com/navoni1024/Mygo_random_frame_bot)", inline=False)
 		embed.set_footer(text="*GIF太大的話，在discord中有可能不會動。")
 		embed.set_image(url="attachment://SOYO.jpg")
@@ -22,11 +22,11 @@ async def load_extensions():
 		if filename.endswith('.py'):
 			await bot.load_extension(f'cogs.{filename[:-3]}')
 
-with open('setting.json','r',encoding='utf8') as jfile:
+with open('setting_.json','r',encoding='utf8') as jfile:
 	jdata = json.load(jfile)
 
 intents = discord.Intents.default()
-intents.members = True
+#intents.members = True
 bot = commands.Bot(command_prefix=commands.when_mentioned_or(jdata['prefix']), intents=intents)
 bot.help_command = MyHelpCommand()
 
